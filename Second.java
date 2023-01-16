fe2a3da74bb1934141715e80747822d1be0a4391
Shani Kumar <shanikumar@Shanis-MacBook-Air.local>
2023-01-16 11:21:00 +0530

    second

---
+++ Second.java
@@ -0,0 +1,21 @@
+class Main{
+  private  int  m=12;
+  void prin(){
+    System.out.println(m);
+  }
+ static void main(String args[]){
+    
+    
+    // int c=Main.add(num1, num2);
+    // System.out.println(c);
+}
+}
+class Sum{
+    public static void main() {
+       
+        Main obj=new Main();
+        obj.prin();
+    }
+}
+    
+
